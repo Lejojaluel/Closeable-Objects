98d721a6fa97443156ad2b238e635c3a25195231
Lejojaluel <lejojaluel@gmail.com>
2019-10-07 11:51:56 -0600

    Add files via upload
    Adding additional Arraylist to the testing

--- Closeable.java
+++ Closeable.java
@@ -3,106 +3,139 @@
  *
  * @author Leroy Valencia
  */
+/*
+ * Enumeration class for the Container state. Either OPEN or CLOSE
+ */
 enum ContainerState {
-    OPEN, CLOSE;
+  OPEN,
+  CLOSE;
 
-    public String toString(ContainerState state) {
-        switch (state) {
-            case OPEN:
-                return "Opened";
-            case CLOSE:
-                return "Closed";
-            default:
-                System.err.println("Impossible state in the ContainerState Enum");
-                System.exit(1);
-        }
-        return "Impossible Reach in the end of the switch in the enum.";
+  public String toString(ContainerState state) {
+    switch (state) {
+      case OPEN:
+        return "Opened";
+      case CLOSE:
+        return "Closed";
+      default:
+        System.err.println("Impossible state in the ContainerState Enum");
+        System.exit(1);
     }
+    return "Impossible Reach in the end of the switch in the enum.";
+  }
 }
 
-
+/**
+ * The Main Generic Class
+ *
+ * @param <T>
+ */
 public class Closeable<T> {
-    boolean v = true;
-    ContainerState state;
-    private T t;
+  boolean v = true;
+  ContainerState state;
+  private T t;
 
+  /**
+   * First Constructor to create a new closeable
+   *
+   * @param item
+   * @param state
+   */
+  Closeable(T item, ContainerState state) {
+    // I had the state after the item so you can put something in the container and then close it.
+    this.t = item;
+    this.state = state;
+    // places item in the container and sets the state to the specified value.
+    // Note you can put something in the container then close it this way.
+  }
 
-    Closeable(T item, ContainerState state) {
-        //I had the state after the item so you can put something in the container and then close it.
-        this.t = item;
-        this.state = state;
-        //places item in the container and sets the state to the specified value.
-        //Note you can put something in the container then close it this way.
+  /** A default null closeable constructor */
+  Closeable() {
+    // puts null and sets state to closed
+    this.t = null;
+    this.state = ContainerState.CLOSE;
+  }
+
+  /**
+   * This method sets the value of what is inside the closeable only when the State is Open. The
+   * validation of the T item is handled by the compiler
+   *
+   * @param item
+   */
+  public void setValue(T item) {
+    // If only open otherwise return null
+    if (state == ContainerState.OPEN) {
+      this.t = item;
+      System.out.println("Item is set.");
+    } else if (state == ContainerState.CLOSE) {
+      // ignore the item
+      System.err.println("Item did not set because the container is closed.");
     }
+  }
 
-    Closeable() {
-        //puts null and sets state to closed
-        this.t = null;
-        this.state = ContainerState.CLOSE;
+  /**
+   * A getter for the value. Returns the value only when the state is OPEN.
+   *
+   * @return the T value of the container when open.
+   */
+  public T getValue() {
+    // If only open otherwise return null
+    if (state == ContainerState.OPEN) {
+      return t;
+    } else {
+      System.err.println("Cannot get value/s when container is closed.\nReturning null...");
+      return null;
     }
+  }
 
-    public void setValue(T item) {
-        //If only open otherwise return null
-        if (state == ContainerState.OPEN) {
-            this.t = item;
-            System.out.println("Item is set.");
-        } else if (state == ContainerState.CLOSE) {
-            //ignore the item
-            System.err.println("Item did not set because the container is closed.");
-        }
+  /**
+   * This method returns the state that the container is in. Not the string verison.
+   *
+   * @return the ContainerState value of the container.
+   */
+  public ContainerState getState() {
+    return state;
+    // returns whether or not the container is open.
+  }
 
+  /** This method opens the container for writing and reading */
+  public void open() {
+    state = ContainerState.OPEN;
+    verbose("Container " + state.toString(getState()));
+    // set the container state to OPEN.
+  }
+
+  /** This method closes the container. No reading or writing. */
+  public void close() {
+    state = ContainerState.CLOSE;
+    verbose("Container " + state.toString(getState()));
+    // set the container state to CLOSE.
+  }
+
+  /**
+   * A method that returns a string printout of information of the container.
+   *
+   * @return A string of the information of the container.
+   */
+  public String toString() {
+    if (state == ContainerState.OPEN) {
+      return "This container that is in state "
+          + getState().toString()
+          + " that has the value/s "
+          + getValue();
+    } else {
+      return "";
     }
+  }
 
-    public T getValue() {
-        //If only open otherwise return null
-        if (state == ContainerState.OPEN) {
-            return t;
-        } else {
-            System.err.println("Cannot get value when container is closed.\nReturning null...");
-            return null;
-        }
+  /**
+   * Prints stuff to verbose
+   *
+   * @param stuff inputted type to print
+   * @param <V> generic type
+   */
+  public <V> void verbose(V stuff) {
+    if (v) {
+      System.err.println(stuff);
     }
-
-
-    public ContainerState getState() {
-        return state;
-        //returns whether or not the container is open.
-    }
-
-    public void open() {
-        state = ContainerState.OPEN;
-        verbose("Container " + state.toString(getState()));
-        //set the container state to OPEN.
-    }
-
-    public void close() {
-        state = ContainerState.CLOSE;
-        verbose("Container " + state.toString(getState()));
-        //set the container state to CLOSE.
-    }
-
-    public String toString() {
-        if (state == ContainerState.OPEN) {
-            return "This container that is in state " + getState().toString() + " that has the value " + getValue();
-        } else {
-            return "";
-        }
-    }
-
-
-    /**
-     * Prints stuff to verbose
-     *
-     * @param stuff inputted type to print
-     * @param <V>   generic type
-     */
-    public <V> void verbose(V stuff) {
-        if (v) {
-            System.err.println(stuff);
-        }
-    }
-
-
+  }
 }
-
-
--- CloseableTest.java
+++ CloseableTest.java
@@ -1,20 +1,45 @@
+/** Testing class for Closeable @Author Leroy Valencia */
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class CloseableTest {
-    public static void main(String[] args) {
-        //Create a version that stores an Integer.  Test putting a value in and getting it out, both when open and closed.
-        //Print the results as you try the various operations.
-        System.out.println("Testing Integer Closeable...");
-        Closeable<Integer> zipInt = new Closeable<>(69, ContainerState.OPEN);
-        zipInt.open();
-        System.out.println("getState() = " + zipInt.getState());
-        zipInt.setValue(69);
-        System.out.println("getValue() = " + zipInt.getValue());
-        System.out.println(zipInt.toString());
-        zipInt.close();
-        System.out.println("getState() = " + zipInt.getState());
-        System.out.println("getValue() = " + zipInt.getValue());
-        zipInt.setValue(40); //This should fail
-        System.out.println(zipInt.toString());
+  public static void main(String[] args) {
+    // Create a version that stores an Integer.  Test putting a value in and getting it out, both
+    // when open and closed.
+    // Print the results as you try the various operations.
+    System.out.println("Testing Integer Closeable...");
+    Closeable<Integer> zipInt = new Closeable<>(69, ContainerState.OPEN);
+    zipInt.open();
+    System.out.println("getState() = " + zipInt.getState());
+    zipInt.setValue(69);
+    System.out.println("getValue() = " + zipInt.getValue());
+    System.out.println(zipInt.toString());
+    zipInt.close();
+    System.out.println("getState() = " + zipInt.getState());
+    System.out.println("getValue() = " + zipInt.getValue());
+    zipInt.setValue(40); // This should fail
+    System.out.println(zipInt.toString());
 
-
-    }
+    // Arraylist of Strings test
+    System.out.println("Testing ArrayList of Strings Closeable...");
+    ArrayList<String> arr = new ArrayList<String>();
+    arr.add("pencil");
+    arr.add("paper");
+    arr.add("pen");
+    arr.add("sharpie");
+    arr.add("highlighter");
+    arr.add("notebook");
+    arr.add("note");
+    Closeable<ArrayList> zipArr = new Closeable<ArrayList>(arr, ContainerState.OPEN);
+    zipArr.open();
+    System.out.println("getState() = " + zipArr.getState());
+    // zipInt.setValue(69);
+    System.out.println("getValue() = " + zipArr.getValue());
+    System.out.println(zipArr.toString());
+    zipArr.close();
+    System.out.println("getState() = " + zipArr.getState());
+    System.out.println("getValue() = " + zipArr.getValue());
+    // zipArr.setValue(40); // This should fail
+    System.out.println(zipArr.toString());
+  }
 }
